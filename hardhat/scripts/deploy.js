924d939738a6b608f550831e1f142d0508211450
Ramiro Aragon Paz <ramiroaragonpaz@gmail.com>
2022-10-05 15:21:40 -0300

    nft-ipfs

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,20 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({path: ".env"});
+
+async function main() {
+  const metadataURL = "ipfs://QmcvoPxo1yrpXBp2eQrbSLywgi8hDxQ8ZZeH3ND3651nqg/"
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+  await deployedLW3PunksContract.deployed();
+
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+
+  
+}
+
+main()
+  .then(()=> process.exit(0))
+  .catch((error)=>{
+    console.error(error);
+    process.exit(1);
+  });
